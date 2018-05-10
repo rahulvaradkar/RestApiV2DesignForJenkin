b7b5969dac6816bcc84f398b162e611ad41b20c4
pavand1 <pavan.divekar@yahoo.in>
2018-05-10 10:49:46 +0530

    demo test file is added

---
+++ src/test/java/MyTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+public class MyTest extends TestCase {
+	@Test  
+    public void testFindMax(){  
+        assertEquals(4,4);  
+        assertEquals(-1,-1);  
+    }
+}
