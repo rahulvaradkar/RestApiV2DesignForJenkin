5506c1ec8837acef7bc22b55384cb84cb8f38528
Siddhant Chothe <sidnc86@gmail.com>
2018-05-24 19:10:41 +0530

    reorganized client-side code and got qunit test running

--- src/test/rest-api/scripts/tests.js
+++ src/test/rest-api/scripts/tests.js
@@ -1,21 +1,4 @@
-QUnit.test( "hello test", function( assert ) {
-    assert.ok( 1 == "1", "Passed!" );
-  });
-  QUnit.test( "Getting users", function( assert ) {
-    var done = assert.async();
-    var input = $( "#test-input" ).focus();
-    $.ajax({url: "http://localhost:8080/BAE/rest/user?active=true",
-          type: "GET",
-          dataType: "json",
-          success: function(result){
-              console.log(result);
-              console.log(JSON.stringify(result));
-              console.log(JSON.parse(JSON.stringify(result)));
-          assert.equal(result.length,6,"Retrieved the JSON");
-          done();
-      }});
-  });
-  QUnit.test( "Getting neighborhood", function( assert ) {
+  QUnit.test( "Posting new Neighborhood", function( assert ) {
     var done = assert.async();
     var data = {
         "level":"-1",
@@ -24,10 +7,34 @@ QUnit.test( "hello test", function( assert ) {
 	"parentId": "-1",
 	"secure": "false"
     }
-    $.ajax({url: "http://localhost:8080/BAE/rest/neighborhood",
+    $.ajax({url: Globals.baseURL + "rest/neighborhood",
           type: "POST",
           dataType: "json",
-          data: data,
+          data: JSON.stringify(data),
+          contentType: "application/json",
+          success: function(result){
+              console.log(result);
+              console.log(JSON.stringify(result));
+              console.log(JSON.parse(JSON.stringify(result)));
+          assert.notEqual(result.length,0,"object should not be empty !");
+          assert.equal(Object.keys(result[0]).length, 5,"neighborhood should have 5 properties !");
+          done();
+      }});
+  });
+  QUnit.test( "Posting new User", function( assert ) {
+    var done = assert.async();
+    var data = {
+        "firstName":"sachin",
+        "lastName": "tendulkar",
+        "password":"sachin123",
+        "email": "sachin@gmail.com",
+        "externalid": "sachin",
+        "id":"0"
+    }
+    $.ajax({url: Globals.baseURL + "rest/user",
+          type: "POST",
+          dataType: "json",
+          data: JSON.stringify(data),
           contentType: "application/json",
           success: function(result){
               console.log(result);
