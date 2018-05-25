bb0a04e762c7e1f11b4ae384258320f5d0b29e13
Siddhant Chothe <sidnc86@gmail.com>
2018-05-25 15:47:14 +0530

    Check valid URL in globals.js testcase written in test.js

--- src/test/rest-api/scripts/tests.js
+++ src/test/rest-api/scripts/tests.js
@@ -1,11 +1,28 @@
+//test to ensure right URL is being accessed
+  QUnit.test("Checking for valid URL", function(assert) {
+    var done = assert.async();
+    var requestSuccessful = false;
+    $.ajax({url: Globals.baseURL + "rest/user/1",
+      type: "GET",
+      dataType: "json",
+      contentType: "application/json",
+      success: function(result){
+        requestSuccessful = true;
+      },
+      complete: function() {
+        assert.equal(requestSuccessful, true, "Should have valid baseURL in globals.js.");
+        done();
+      }
+    });
+  });
   QUnit.test( "Posting new Neighborhood", function( assert ) {
     var done = assert.async();
     var data = {
         "level":"-1",
-	"name": "apiTest2",
-	"id":"0",
-	"parentId": "-1",
-	"secure": "false"
+        "name": "apiTest2",
+        "id":"0",
+        "parentId": "-1",
+        "secure": "false"
     }
     $.ajax({url: Globals.baseURL + "rest/neighborhood",
           type: "POST",
