07357a317e8ff18f48e6b187e65694016646d78c
Siddhant Chothe <sidnc86@gmail.com>
2018-05-25 15:03:53 +0530

    Some comments in globals.js

--- src/test/rest-api/scripts/globals.js
+++ src/test/rest-api/scripts/globals.js
@@ -1,3 +1,11 @@
+//All global variables in this file.
+//If any need to commit this file, 
+//1. Remove it from .gitignore first.
+//2. Make changes here and commit them.
+//3. Once committed, ignore it again by 
+//  a. running `git rm --cached src/test/rest-api/scripts/globals.js
+//  b. And then adding this line to .gitignore back again "/src/test/rest-api/scripts/globals.js"
 var Globals = {
+  //change following line to match your test instance URL
   baseURL: "http://localhost:8081/BAE4/"
 };
